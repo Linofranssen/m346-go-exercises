4df6c5a8fe12b3bad4a04d09d765b9d075dc536a
Noah Krummenacher <krummenachernoah@outlook.com>
2024-11-21 15:41:48 +0100

    Ex1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+var firstName = "Noah"
+var lastName = "Krummenacher"
+var dayOfBirth = 26
+var monthOfBirth = 12
+var yearOfBirth = 2005
+var numberOfSiblings = 2
+var heightInMeters = 1.77
+var zodiacSign = '\u2651'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
