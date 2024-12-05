dbe536c70bd4034645ca7ed962665bf101b92253
Noah Krummenacher <krummenachernoah@outlook.com>
2024-12-05 16:22:52 +0100

    go-2-ex4 bis go-3-ex-4

--- go-3-ex-2/main.go
+++ go-3-ex-2/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	Aries       = '\u2648' // Widder
@@ -21,14 +23,36 @@ func outputDateRange(zodiacSign rune) {
 	fmt.Printf("%c: ", zodiacSign)
 	// TODO: Replace if, else if branching with switch/case.
 	// TODO: Define all 12 cases...
-	if zodiacSign == Aries {
-		fmt.Println("21.03. - 20.04")
-	} else if zodiacSign == Taurus {
-		fmt.Println("21.04. - 21.05")
-	} else {
-		fmt.Println("")
-	}
+	switch zodiacSign {
+	case Aquarius:
+		fmt.Print("21.01 - 19.02")
+	case Pisces:
+		fmt.Print("20.02 - 20.03")
+	case Aries:
+		fmt.Print("21.03 - 20.04")
+	case Taurus:
+		fmt.Print("21.04 - 20.05")
+	case Gemini:
+		fmt.Print("21.05 - 21.06")
+	case Cancer:
+		fmt.Print("22.06 - 22.07")
+	case Leo:
+		fmt.Print("23.07 - 23.08")
+	case Virgo:
+		fmt.Print("24.08 - 23.09")
+	case Libra:
+		fmt.Print("24.09 - 23.10")
+	case Scorpius:
+		fmt.Print("24.10 - 22.11")
+	case Sagittarius:
+		fmt.Print("23.11 - 21.12")
+	case Capricornus:
+		fmt.Print("22.12 - 20.01")
+
 	// TODO: ...and consider a default case.
+	default:
+		fmt.Print("Not a valid Zodiac Sign!")
+	}
 }
 
 func main() {
--- go-3-ex-4/main.go
+++ go-3-ex-4/main.go
@@ -22,9 +22,21 @@ const (
 func main() {
 	suits := []rune{Diamonds, Spades, Clubs, Hearts}
 	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+	colors := map[rune]string{
+		Diamonds: "\u001B[31m",
+		Spades:   "\u001B[30m",
+		Clubs:    "\u001B[30m",
+		Hearts:   "\u001B[31m",
+	}
+	resetColor := "\u001B[0m"
 
 	// TODO: Loop over suits and ranks to output all combinations.
+	for _, rank := range ranks {
+		for _, suit := range suits {
+			fmt.Printf("%s%c%s%c\t\t", colors[suit], suit, resetColor, rank)
+		}
+		fmt.Println()
+	}
 
 	// TODO: delete this line afterwards
-	fmt.Println(suits, ranks)
 }
